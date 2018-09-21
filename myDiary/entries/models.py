fc7cf468d9425e806a81b622f3e034ff9528e4b9
j0flintking02 <jonathanaurugai@outlook.com>
2018-09-21 20:12:15 +0300

    update app model

--- myDiary/entries/models.py
+++ myDiary/entries/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    entry_id =models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_created=True)
